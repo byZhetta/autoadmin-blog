d6732e9e51971c1bdda9b88290518068b2475058
ignaciozhetta <zagu4988@gmail.com>
2023-03-16 16:41:15 -0300

    initial config and db disign

---
+++ app/Models/Post.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    // Relation one to much inverse
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category() {
+        return $this->belongsTo(Category::class);
+    }
+
+    // Relation much to much
+    public function tags() {
+        return $this->belongsToMany(Tag::class);
+    }
+
+    // Relation one to one polimorfic
+    public function image() {
+        return $this->morphOne(Imge::class, 'imageable');
+    }
+
+}
